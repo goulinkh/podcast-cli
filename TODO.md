cc537726539141d3c0318125d7a18c6c2ba04da7
Goulin Khoge <Goulin.khoge@gmail.com>
2020-05-04 04:11:22 +0200

    Cosuming Podcasts from Itunes API

---
+++ TODO.md
@@ -0,0 +1,3 @@
+## TODO
+
+- [ ] Implement the search mode with this URL `https://itunes.apple.com/search?country=us&term={query}&entity=podcast`, attention to not add the podcastAuther results
--- temp/temp.go
+++ temp/temp.go
@@ -1,7 +1,7 @@
 package temp
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -14,20 +14,22 @@ import (
 const applePodcastsMainPage = "https://podcasts.apple.com/genre/podcasts/id26"
 
 var (
-	authRegEx      = regexp.MustCompile("22privateKeyPath%22%3A%22ssl%2Fwebplayer.p8%22%2C%22token%22%3A%22(?P<Bearer>.+)%22%7D%2C%22routerScroll")
-	podcastIdRegEx = regexp.MustCompile(`\d+$`)
+	authorization string
 )
 
 type Genre struct {
 	Text     string   `json:"text"`
 	URL      string   `json:"url"`
+	Id       string   `json:"url"`
 	SubGenre []*Genre `json:"sub-genres"`
 }
 
 type Podcast struct {
-	Title string `json:"title"`
-	URL   string `json:"url`
-	Id    string `json:"id"`
+	Title       string `json:"title"`
+	URL         string `json:"url`
+	Id          string `json:"id"`
+	Description string `json:"description"`
+	Author      string `json:"author"`
 }
 type Episode struct {
 	Id                     string `json:"id"`
@@ -39,57 +41,48 @@ type Episode struct {
 	Description            string `json:"description"`
 }
 
-func (g *Genre) String() string {
-	str, err := json.Marshal(g)
-	if err != nil {
-		return ""
-	}
-	return string(str)
-}
-func (g *Genre) getPodcasts() ([]*Podcast, error) {
-	resp, err := http.Get(g.URL)
+func (g *Genre) GetPodcasts() ([]*Podcast, error) {
+	request, err := http.NewRequest("GET", "https://amp-api.podcasts.apple.com/v1/catalog/us/charts?types=podcasts&limit=200&genre="+g.Id, nil)
 	if err != nil {
 		return nil, err
 	}
-	doc, err := goquery.NewDocumentFromResponse(resp)
+	authorization, err := getAuthorization()
 	if err != nil {
 		return nil, err
 	}
-	podcasts := make([]*Podcast, 0)
-	doc.Find("#selectedcontent li a").Each(func(i int, s *goquery.Selection) {
-		href, exists := s.Attr("href")
-		if !exists {
-			return
+	request.Header.Add("Authorization", authorization)
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	podcastsJSON := gjson.Get(string(data), "results.podcasts.0.data").Array()
+	podcasts := make([]*Podcast, len(podcastsJSON))
+	for i, podcast := range podcastsJSON {
+		podcasts[i] = &Podcast{
+			Id:          podcast.Get("id").String(),
+			Description: podcast.Get("attributes.description.standard").String(),
+			Title:       podcast.Get("attributes.name").String(),
+			URL:         "https://amp-api.podcasts.apple.com" + podcast.Get("href").String(),
+			Author:      podcast.Get("attributes.artistName").String(),
 		}
-		podcasts = append(podcasts, &Podcast{
-			Title: s.Text(),
-			URL:   href,
-			Id:    podcastIdRegEx.FindString(href),
-		})
-	})
+	}
 	return podcasts, nil
 }
-func (p *Podcast) getEpisodes() ([]*Episode, error) {
-	response, err := http.Get(p.URL)
-	if err != nil {
-		return nil, err
-	}
-	podcastPageBytes, err := ioutil.ReadAll(response.Body)
 
-	match := authRegEx.FindStringSubmatch(string(podcastPageBytes))
-	if len(match) != 2 {
-		return nil, err
-
-	}
-	authorization := "Bearer " + match[1]
-	fmt.Println(authorization)
+func (p *Podcast) GetEpisodes() ([]*Episode, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://amp-api.podcasts.apple.com/v1/catalog/us/podcasts/%s/episodes?offset=0&limit=300", p.Id), nil)
 	if err != nil {
 		return nil, err
 
 	}
+	authorization, err := getAuthorization()
+	if err != nil {
+		return nil, err
+	}
 	req.Header.Add("Authorization", authorization)
-	response, err = http.DefaultClient.Do(req)
+	response, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 
@@ -99,9 +92,9 @@ func (p *Podcast) getEpisodes() ([]*Episode, error) {
 		return nil, err
 
 	}
-	episodesJson := gjson.Get(string(data), "data").Array()
-	episodes := make([]*Episode, len(episodesJson))
-	for i, episode := range episodesJson {
+	episodesJSON := gjson.Get(string(data), "data").Array()
+	episodes := make([]*Episode, len(episodesJSON))
+	for i, episode := range episodesJSON {
 		episodes[i] = &Episode{
 			Id:                     episode.Get(`id`).String(),
 			Artwork:                episode.Get(`attributes.artwork.url`).String(),
@@ -114,6 +107,12 @@ func (p *Podcast) getEpisodes() ([]*Episode, error) {
 	}
 	return episodes, nil
 }
+
+func getGenreId(url string) string {
+	idRegExp := regexp.MustCompile(`\d+$`)
+	return idRegExp.FindString(url)
+}
+
 func GetGenres() ([]*Genre, error) {
 	resp, err := http.Get(applePodcastsMainPage)
 	if err != nil {
@@ -131,14 +130,14 @@ func GetGenres() ([]*Genre, error) {
 			return
 		}
 
-		genre := &Genre{Text: genreSelection.Text(), URL: href, SubGenre: make([]*Genre, 0)}
+		genre := &Genre{Text: genreSelection.Text(), URL: href, SubGenre: make([]*Genre, 0), Id: getGenreId(href)}
 
 		s.Find(".top-level-subgenres > li > a").Each(func(i int, s *goquery.Selection) {
 			href, exists := genreSelection.Attr("href")
 			if !exists {
 				return
 			}
-			genre.SubGenre = append(genre.SubGenre, &Genre{Text: s.Text(), URL: href})
+			genre.SubGenre = append(genre.SubGenre, &Genre{Text: s.Text(), URL: href, Id: getGenreId(href)})
 		})
 		genres = append(genres, genre)
 
@@ -146,14 +145,23 @@ func GetGenres() ([]*Genre, error) {
 	return genres, nil
 }
 
-func Main() {
-	/* genres, _ := getGenres()
-	podcasts, _ := genres[0].SubGenre[0].getPodcasts()
-	res, _ := json.Marshal(podcasts)
-	fmt.Println(string(res))
-	fmt.Println((genres[0].SubGenre[0])) */
-	genres, _ := GetGenres()
-	podcasts, _ := genres[0].SubGenre[0].getPodcasts()
-	episodes, _ := podcasts[0].getEpisodes()
-	fmt.Println(len(episodes))
+func getAuthorization() (string, error) {
+	if authorization != "" {
+		return authorization, nil
+	}
+	authRegEx := regexp.MustCompile("22privateKeyPath%22%3A%22ssl%2Fwebplayer.p8%22%2C%22token%22%3A%22(?P<Bearer>.+)%22%7D%2C%22routerScroll")
+	resp, err := http.Get("https://podcasts.apple.com/us/podcast/the-joe-rogan-experience/id360084272")
+	if err != nil {
+		return "", err
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	match := authRegEx.FindStringSubmatch(string(data))
+	if len(match) != 2 {
+		return "", errors.New("Authorization access token is not found")
+	}
+	authorization = "Bearer " + match[1]
+	return authorization, nil
 }
